2e2921fb83621bc4a5db3fa05ddf2bd3ed8f2845
AUNGKYAWCHA MARMA <aungkyawcha@AUNGKYAWCHAsMBP.home>
2019-10-25 16:10:58 -0400

    HTTP request

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 require('dotenv').config(); // laod env variables form .env file to process.env
 const request = require('request');
 
-console.log(process.env);
+const key = process.env.DARK_SKY_NET_API_KEY;
+const url = `https://api.darksky.net/forecast/${key}/42.3601,-71.0589?lang=bn`;
 
-// const url = 'https://api.darksky.net/forecast/3282dbfba449ef0e498ee412162f44e8/42.3601,-71.0589';
-
-// request.get({ url: url }, (error, response) => {
-// 	if (error) {
-// 		console.log('ERROR');
-// 		console.log(error);
-// 	} else {
-// 		console.log('RESPONSE........');
-// 		const data = JSON.parse(response.body);
-// 		console.log('data.currently', data.currently);
-// 	}
-// });
-// darksky.net api key 3282dbfba449ef0e498ee412162f44e8
+request.get({ url: url, json: true }, (error, response) => {
+	if (error) {
+		console.log('ERROR...');
+		console.log(error);
+	} else {
+		console.log('RESPONSE........');
+		const { body } = response;
+		console.log(body);
+	}
+});
